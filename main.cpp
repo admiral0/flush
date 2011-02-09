68c66f1a31ba615044d75f15465e4c750c6ebb9f
Radu Andries <admiral0@tuxfamily.org>
2011-02-09 02:23:42 +0100

    Working version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <QCoreApplication>
-#include "flush.h"
 #include <QProcess>
-
-
+#include <phonon/MediaObject>
+#include <phonon/MediaSource>
+#include <phonon/AudioOutput>
 int main(int argc, char** argv)
 {
     QCoreApplication app(argc, argv);
@@ -12,8 +12,17 @@ int main(int argc, char** argv)
     QString appl=args.at(0);
     args.removeFirst();
     QProcess p;
-    p.startDetached(appl,args);
+    p.setProcessChannelMode(QProcess::ForwardedChannels);
+    p.start(appl,args);
     p.waitForFinished(-1);
-    Flush f(p.exitCode());
+    Phonon::MediaObject *mediaObject = new Phonon::MediaObject();
+    Phonon::AudioOutput *audioOutput = new Phonon::AudioOutput(Phonon::MusicCategory);
+    Phonon::createPath(mediaObject, audioOutput);
+    if(p.exitCode()==0)
+      mediaObject->setCurrentSource(Phonon::MediaSource(":/flush.ogg"));
+    else
+      mediaObject->setCurrentSource(Phonon::MediaSource(":/sadtrombone.ogg"));
+    mediaObject->play();
+    QObject::connect(mediaObject,SIGNAL(finished()),&app,SLOT(quit()));
     return app.exec();
 }
